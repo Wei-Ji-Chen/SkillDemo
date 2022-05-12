b805c29346047b2372089894123b446797a55b38
Weiji Chen <wjchen@ucsd.edu>
2022-05-11 17:54:17 -0700

    commit

---
+++ SkillDemoTest.java
@@ -0,0 +1,12 @@
+import java.beans.Transient;
+
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test 
+    public void subtract(){
+        assertEquals(4, SkillDemo.subtract(5,2)); 
+    }
+    
+}
